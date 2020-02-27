ee115b6862bafe6756e49846e546fbb21d24cdca
Franco <alvarez.fr893@gmail.com>
2020-02-27 16:06:11 -0300

    primer commit

---
+++ services-ipau-usuarios/src/main/java/com/app/ipau/usuarios/ServicesIpauUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.app.ipau.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesIpauUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesIpauUsuariosApplication.class, args);
+	}
+
+}
---
+++ services-ipau-usuarios/src/test/java/com/app/ipau/usuarios/ServicesIpauUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.ipau.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesIpauUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
