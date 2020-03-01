f2f70de4c87f50f8817a9dc08a48bb17698f2700
Franco <alvarez.fr893@gmail.com>
2020-03-01 10:10:16 -0300

    se configuro para ejecutar de forma local

--- services-ipau-usuarios/src/main/java/com/app/ipau/usuarios/ServicesIpauUsuariosApplication.java
+++ services-ipau-usuarios/src/main/java/com/app/ipau/usuarios/ServicesIpauUsuariosApplication.java
@@ -2,8 +2,12 @@ package com.app.ipau.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
+@EntityScan({"com.app.ipau.commons.usuarios.models.entities"})
 public class ServicesIpauUsuariosApplication {
 
 	public static void main(String[] args) {
